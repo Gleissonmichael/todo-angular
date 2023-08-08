a87c136e9dd53167edb4d82c44a021753bd2439d
Gleisson Santos <gleissonmichael1@gmail.com>
2023-08-07 21:07:07 -0300

    feat: estilos

--- src/app/interfaces/todo.ts
+++ src/app/interfaces/todo.ts
@@ -2,7 +2,7 @@ export interface Todo {
   id: number;
   name: string;
   category: string;
-  isChecked: boolean;
+  isCompleted: boolean;
 }
 
 export interface TodosResult {
--- src/app/mock-todos.ts
+++ src/app/mock-todos.ts
@@ -1,7 +1,7 @@
 import { Todo } from './interfaces/todo';
 
 export const TODOS: Todo[] = [
-  { id: 1, name: 'Todo 1', category: 'Development', isChecked: false },
-  { id: 2, name: 'Todo 2', category: 'Development', isChecked: true },
-  { id: 3, name: 'Todo 3', category: 'Development', isChecked: false },
+  { id: 1, name: 'Todo 1', category: 'Development', isCompleted: false },
+  { id: 2, name: 'Todo 2', category: 'Development', isCompleted: true },
+  { id: 3, name: 'Todo 3', category: 'Development', isCompleted: false },
 ];
